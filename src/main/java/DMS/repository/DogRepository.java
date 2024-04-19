2f2681818fa1f1319068eb3d46301dd08e0c0fc8
Naresh <143207403+Chityala201@users.noreply.github.com>
2024-04-19 14:07:02 -0500

    Added my project

---
+++ src/main/java/DMS/repository/DogRepository.java
@@ -0,0 +1,11 @@
+package DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog,Integer>{
+	List<Dog> findByName(String name);
+}
---
+++ src/main/java/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
